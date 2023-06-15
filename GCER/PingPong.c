570806444758087ebd26e17c1dc7a7dd79cfda7a
dkbada <119907191+dkbada@users.noreply.github.com>
2023-06-14 21:05:20 -0400

    Update PingPong.c

--- GCER/PingPong.c
+++ GCER/PingPong.c
@@ -1,189 +1,203 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <kipr/wombat.h>
 
-// Constants
+typedef int bool;
+#define true 1
+#define false 0
+#define until(x) while (!(x))
+#define puts(x) printf(x "\n")
+
+#define L_MOTOR 0
+#define R_MOTOR 1
+void move(int l_power, int r_power) {
+    motor(L_MOTOR, l_power);
+    motor(R_MOTOR, r_power);
+}
+#define stop() move(0, 0)
+
+void drive_for(int l_power, int r_power, int sleep_time) {
+    move(l_power, r_power);
+    msleep(sleep_time);
+    move(0, 0);
+}
+
+void slow_servo(int which, int to) {
+    int from = get_servo_position(which), i;
+    if (from < to) {
+        for (i = from; i < to; i++) {
+            set_servo_position(which, i);
+            msleep(1);
+        }
+    } else {
+        for (i = from; i > to; i--) {
+            set_servo_position(which, i);
+            msleep(1);
+        }
+    }
+}
+
+// Line Following sensors and constants
+#define LS_R 2
+#define LS_L 1
+#define on_black(ls) (analog(ls) > BLACK)
 #define BLACK 1500
-#define BACK_BLACK 3700
+void line_follow(int aggression) {
+    if (on_black(LS_L)) move(aggression, 100);
+    else if (on_black(LS_R)) move(100, aggression);
+    else move(90, 100);
+}
+#define line_follow_until(cond, aggression) do { \
+    line_follow(aggression); \
+    printf("line follow until " #cond "\n"); \
+    msleep(100); \
+} while (!(cond))
 
-#define SLOW 125
-#define FAST 400
+typedef enum { LEFT, RIGHT } edge_t;
+void line_follow_one_sided(int aggression, int sensor, edge_t edge) {
+    if (on_black(sensor) ^ (edge == LEFT)) move(100, aggression);
+    else move(aggression, 100);
+}
+#define line_follow_one_sided_until(cond, side, edge, aggression) do { \
+    line_follow_one_sided(aggression, side, edge); \
+    printf("line follow one sided until " #cond "\n"); \
+    msleep(100); \
+} while (!(cond))
 
-#define CLAW 1
-#define ARM 0
-
-#define CLAW_HOME 1000
-#define ARM_HOME 2000
-
-#define CLAW_CLOSE 2024
-#define ARM_UP 0
-
-#define LS 1
-
-// Function prototypes
-void stop();
-void pause(int time);
-void servo_control(int servo, int end_pos, int tick_delay);
-void drive(int left_power, int right_power, int duration);
-void wfl();
-void main_program();
-
-// Main program
-int main()
-{
-    main_program();
-    return 0;
+// Line Counter Sensor
+#define LC_BLACK 2700
+bool lc_on_black(int threshold) {
+    return analog(3) > threshold;
 }
 
-// Function definitions
+// Servos
+#define DOOR_SERVO 1
+typedef enum { D_CLOSED = 600, D_OPEN = 0 } door_pos_t;
+void ball_door(door_pos_t pos) { set_servo_position(DOOR_SERVO, pos); msleep(400); }
 
-void stop()
-{
-    // All off (also K.create_stop())
-    drive(0, 0, 0);
-}
+#define ARM_SERVO 0
+typedef enum { FLOOR = 2047, DOWN = 1970, WIRESHARK = 1860, UP = 1300, OUT_OF_WAY = 1500 } tube_arm_pos_t;
+void tube_arm(tube_arm_pos_t pos) { slow_servo(ARM_SERVO, pos); }
 
-void pause(int time)
-{
-    stop();
-    // Delay for the specified time
-    if (time > 0)
-        usleep(time * 1000);
-}
+#define GRABBER_SERVO 2
+typedef enum { G_OPEN = 0, G_CLOSED = 1160 } grabber_pos_t;
+void grabber(grabber_pos_t pos) { slow_servo(GRABBER_SERVO, pos); }
 
-void servo_control(int servo, int end_pos, int tick_delay)
-{
-    // Function for using servo
-    int pos = get_servo_position(servo);
-    printf("%d %d %d\n", servo, pos, end_pos);
-    if (pos > end_pos)
-    {
-        for (int i = pos; i > end_pos; i -= 2)
-        {
-            set_servo_position(servo, i);
-            pause(tick_delay);
-        }
+void jiggle() {
+    int i;
+    for (i = 0; i < 20; i++) {
+        puts("jiggle jiggle jiggle");
+        move(100, -100);
+        msleep(25);
+        move(-100, -100);
+        msleep(25);
+        move(-100, 100);
+        msleep(25);
+        move(100, 100);
+        msleep(25);
     }
-    else
-    {
-        for (int i = pos; i < end_pos; i += 2)
-        {
-            set_servo_position(servo, i);
-            pause(tick_delay);
-        }
-    }
+    move(0, 0);
 }
 
-void drive(int left_power, int right_power, int duration)
-{
-    // Drive for n milliseconds
-    create_drive_direct(left_power, right_power);
-    pause(duration);
-    stop();
+// Switch sensors
+bool wireshark_switch() { return digital(9); }
+bool tube_switch() { return digital(8); }
+
+void sleep_until_balls_drop(double start) {
+    puts("sleeping until balls");
+    while (seconds() - start < 45) { ////////////////////////////////////////////////////////FIXME//////////
+        printf("time: %lg seconds\n", seconds() - start);
+        msleep(100);
+    }
+    puts("at 61 seconds");
 }
 
-void wfl()
-{
-    // Wait for start light
-    int START = analog(LS);
-    while (analog(LS) > START / 2)
-    {
-        pause(50);
-    }
-}
-
-void main_program()
-{
-    // Create Setup
-    int test = 0;
-    int loop = 0;
-
-    printf("Press A to continue\n");
-    printf("Press B to test\n");
-    while (!loop)
-    {
-        if (a_button())
-        {
-            printf("Continuing\n");
-            loop = 1;
-        }
-        else if (b_button())
-        {
-            test = 1;
-            printf("Testing\n");
-            loop = 1;
-        }
-    }
-
-    printf("Turn on Create\n");
-    create_connect();
-    printf("Create Connected!\n");
-
-    printf("Walaby Battery = %d\n", power_level());
-    printf("Create Battery = %d\n", get_create_battery_capacity());
+void setup() {
     enable_servos();
-    printf("Servos Enabled\n");
-    set_servo_position(ARM, ARM_HOME);
-    set_servo_position(CLAW, CLAW_HOME);
+    ball_door(D_CLOSED);
+    tube_arm(UP);
+    grabber(G_OPEN);
+    drive_for(-100, -100, 300);
+}
 
-    printf("Wait for Start Light\n");
+void wfl() {
+    puts("waiting for light");
+    int initial = analog(0);
+    until (analog(0) < (initial >> 1)) msleep(100);
+    puts("start!!");
+}
+
+int main() {
+    setup();
     wfl();
-    shut_down_in(118)
-    double start_time = seconds();
-
-    // Do stuff
-
-    // Drive to middle line
-    int lw = -FAST;
-    int rw = -FAST;
-    create_drive_direct(lw, rw);
-    pause(1200); // Skip over first black line and knock over ring toy
-    set_servo_position(ARM, 1000);
-
-    // Look for Middle Line
-    while ((lw > 0) && (rw > 0))
-    {
-        if (get_create_lcliff_amt() < 2000)
-        {
-            lw = 0;
-            rw = 0;
+    double start = seconds();
+    //    shut_down_in(118.);
+    puts("Going to line");
+    until (on_black(LS_L) && on_black(LS_R)) move(100, 100);
+    drive_for(100, 100, 1000);
+    puts("turning");
+    until (on_black(LS_R)) move(50, -50);
+    // Go to wireshark
+    line_follow_until(wireshark_switch(), 70);
+    puts("at wireshark");
+    drive_for(-100, -100, 200);
+    puts("grabbing");
+    move(20, 0);
+    grabber(G_CLOSED);
+    // Turn around
+    drive_for(50, -50, 500);
+    puts("turning around with wireshark");
+    until (on_black(LS_R)) move(50, -50);
+    until (on_black(LS_L)) move(50, -50);
+    drive_for(0, 100, 500);
+    // Linefollow down, counting the right side lines
+    puts("linefollowing down");
+    int lines = 0;
+    bool on_black = false;
+    while (lines < 2) {
+        line_follow(70);
+        if (lc_on_black(LC_BLACK + (on_black ? -20 : 20)) != on_black) { // Implement some hysteresis
+            if (on_black) {
+                lines++;
+                printf("line number %i\n", lines);
+            }
+            on_black = !on_black;
         }
     }
-    pause(500);
-
-    // Rotate 45 degrees Counter-Clockwise
-    drive(-SLOW, SLOW, 900);
-    pause(500);
-
-    // Drive to Center Pipe
-    create_drive_direct(-lw, -rw);
-    pause(1000);
-    pause(500);
-
-    // Get Botgal
-    set_servo_position(ARM, ARM_UP);
-    pause(500);
-    set_servo_position(CLAW, CLAW_CLOSE);
-    pause(500);
-    set_servo_position(ARM, ARM_HOME);
-    pause(500);
-
-    // Drive Straight to Drop Position
-    create_drive_direct(FAST, FAST);
-    pause(1200);
-    pause(500);
-
-    // Rotate 135 degrees Counter-Clockwise
-    drive(-SLOW, SLOW, 1200);
-    pause(500);
-
-    // Drop Botgal
-    set_servo_position(CLAW, CLAW_HOME);
-    pause(500);
-
-    // Clean up
-    create_stop();
-    disable_servos();
-    create_disconnect();
-    printf("Tada\n");
+    // Jump out
+    drive_for(100, 0, 300);
+    // Go to tube
+    tube_arm(DOWN);
+    puts("linefollow one sided");
+    line_follow_one_sided_until(tube_switch(), LS_L, RIGHT, 50);
+    stop();
+    tube_arm(FLOOR);
+    sleep_until_balls_drop(start);
+    jiggle();
+    tube_arm(DOWN);
+    puts("backing up");
+    until (lc_on_black(LC_BLACK)) move(-100, -100);
+    tube_arm(OUT_OF_WAY);
+    puts("turning out of way");
+    drive_for(0, 100, 1500);
+    stop();
+    grabber(G_OPEN);
+    drive_for(-60, -100, 1500);
+    drive_for(100, 100, 300);
+    tube_arm(WIRESHARK);
+    ball_door(D_OPEN);
+    msleep(1000);
+    tube_arm(UP);
+    puts("grabbing wireshark again");
+    drive_for(70, 100, 1800);
+    grabber(G_CLOSED);
+    puts("done");
+    puts("clean up");
+    //drive_for(-50, -100, 2000);
+    stop();
+    tube_arm(OUT_OF_WAY);
+    msleep(100);
+    set_servo_position(GRABBER_SERVO, G_CLOSED);
+    msleep(400);
+    set_servo_position(ARM_SERVO, FLOOR);
+    msleep(100);
+    return 0;
 }
