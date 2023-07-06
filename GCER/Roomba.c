ae6edb2abcc1c4381db701bc4105741bb5729652
dkbada <119907191+dkbada@users.noreply.github.com>
2023-07-05 21:34:32 -0400

    Update Roomba.c

--- GCER/Roomba.c
+++ GCER/Roomba.c
@@ -10,7 +10,7 @@ typedef int bool;
 #define move(l_power, r_power) create_drive_direct(l_power, r_power)
 #define stop() move(0, 0)
 
-void drive_for(int l_power, int r_power, int sleep_time) {
+void drive(int l_power, int r_power, int sleep_time) {
     move(l_power, r_power);
     msleep(sleep_time);
     move(0, 0);
@@ -36,34 +36,20 @@ typedef enum { G_TOWER = 0, G_TUBE = 2047 } grabber_pos_t;
 void grabber_turn(grabber_pos_t pos) { set_servo_position(GRABBER_TURN_SERVO, pos); msleep(400); }
 
 #define ARM_SERVO 3
-typedef enum { DOWN = 0, UP = 1050, BOTGAL_LOW_CUBES = 2047 } arm_pos_t;
+typedef enum { DOWN = 0, UP = 1050, BOTGAL_LOW_CUBES = 2047, DROP = 746 } arm_pos_t;
 void arm(arm_pos_t pos) { slow_servo(ARM_SERVO, pos, 1); }
 
-#define TUBE_PUSH_SERVO 2
-typedef enum { OUT = 1490, START = 2047, IN = 1600 } tube_pos_t;
-void tube_push(tube_pos_t pos) { slow_servo(TUBE_PUSH_SERVO, pos, 1); }
+#define CLAW_SERVO 2
+typedef enum { OPEN = 1024, CLOSED = 0 } claw_pos_t;
+void claw(claw_pos_t pos) { set_servo_position(CLAW_SERVO, pos); msleep(500); }
 
-#define CLAW_PSEUDO_SERVO 0
-#define CLAW_ENCODER 5
-thread pst;
-int target_position = 4070;
-#define Kp 2
-void _control_claw() {
-    while (true) {
-        int power = Kp * (analog(CLAW_ENCODER) - target_position);
-        power = clamp(power, -1500, 1500);
-        mav(CLAW_PSEUDO_SERVO, power);
-    }
-}
-void start_controlling_claw() {
-    pst = thread_create(_control_claw);
-    thread_start(pst);
-}
-void done_controlling_claw() {
-    thread_destroy(pst);
-}
-typedef enum { OPEN = 4080, CLOSED = 200 } claw_pos_t;
-void claw(claw_pos_t pos) { target_position = pos; msleep(500); }
+//grab cube from towers
+void grabby () {
+    claw(CLOSED);
+    arm(DROP);
+    grabber_turn(G_TUBE);
+    claw(OPEN);
+        
 
 void wfl() {
     puts("waiting for light");
@@ -76,25 +62,97 @@ void setup() {
     enable_servos();
     puts("turn on Create");
     //create_connect();
-    tube_push(OUT);
+    //tube_push(OUT);
     arm(UP);
-    start_controlling_claw();
     claw(CLOSED);
     arm(DOWN);
 }
 
+#define FAST 600
+#define SLOW 125
 int main() {
     setup();
     //wfl();
     shut_down_in(118);
-    arm(UP);
-	int i;
-    for (i = 0; i < 4; i++) {
-        msleep(1000);
-        claw(CLOSED);
-        msleep(1000);
-        claw(OPEN);
+    //tube push 
+    
+    //drive to center line
+    move(-FAST, -FAST);
+    msleep(2000);
+    while (get_create_lcliff_amt() > 2000) {
+        move(-FAST, -FAST);
     }
-	done_controlling_claw();
+    arm(UP);
+    grabber_turn(G_TOWER);
+    claw(OPEN);
+    msleep(100); 
+    
+    //rotate 45 deg
+    drive(-SLOW, SLOW, 900);
+    msleep(100);
+    drive(-FAST, -FAST, 900);
+    msleep(100);
+    
+    //grab botgal and go to drop
+    claw(CLOSED);
+    arm(DROP);
+    //might have to wait for other bot
+    drive(FAST, FAST, 950);
+    msleep(500);
+    drive(SLOW, -SLOW, 550);
+    msleep(500);
+    arm(UP);
+    claw(OPEN);
+    msleep(500);
+    
+    //come back and go to next tower
+    drive(-SLOW, SLOW, 600);
+    msleep(100);
+    drive(-FAST, -FAST, 1960);
+    msleep(100);
+    drive(SLOW, SLOW, 500);
+    drive(SLOW, -SLOW, 1250);
+    msleep(100);
+    
+    //get cube 1
+    while (get_create_lfcliff_amt() > 2000) {
+    	move(-SLOW, -SLOW);
+    }
+    drive(-SLOW, -SLOW, 2000);
+    msleep(300);
+    drive(-SLOW, SLOW, 1250);
+    drive(-SLOW, -SLOW, 1000);
+    msleep(1500);
+    drive(SLOW, SLOW, 300);
+    grabby();
+    
+    //go to center pipe
+    drive(SLOW, SLOW, 500);
+    drive(SLOW, -SLOW, 1250);
+    while (get_create_lfcliff_amt() > 2000) {
+        move(SLOW, SLOW);
+    }
+    
+    //grab cube 2
+    drive(SLOW, SLOW, 2000);
+    msleep(300);
+    drive(-SLOW, SLOW, 1250);
+    drive(-SLOW, -SLOW, 1000);
+    msleep(300);
+    drive(SLOW, SLOW, 300);
+    grabby();
+    
+    //grab cube 3 (lower cube)
+    
+    
+    // back to analysis lab
+    
+    
+    //noodles
+    
+    
+    //push out cubes
+        
+    }
     return 0;
 }
