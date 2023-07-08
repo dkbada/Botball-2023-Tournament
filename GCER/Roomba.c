bb4c696bd25b3c4d0e9fb83aeffc39ccb5814e1e
dkbada <119907191+dkbada@users.noreply.github.com>
2023-07-08 16:07:38 -0400

    Update Roomba.c

--- GCER/Roomba.c
+++ GCER/Roomba.c
@@ -32,19 +32,20 @@ void slow_servo(int which, int to, int delay) {
 }
 
 #define GRABBER_TURN_SERVO 0
-typedef enum { G_TOWER = 0, G_TUBE = 2047 } grabber_pos_t;
+typedef enum { G_TOWER = 0, G_TUBE = 2047, G_START = 1552} grabber_pos_t;
 void grabber_turn(grabber_pos_t pos) { set_servo_position(GRABBER_TURN_SERVO, pos); msleep(400); }
 
 #define ARM_SERVO 3
-typedef enum { DOWN = 0, UP = 1050, BOTGAL_LOW_CUBES = 2047, DROP = 746 } arm_pos_t;
+typedef enum { DOWN = 652, UP = 1691, BOTGAL_LOW_CUBES = 2047, DROP = 746, STOW = 1320 } arm_pos_t;
 void arm(arm_pos_t pos) { slow_servo(ARM_SERVO, pos, 1); }
 
 #define CLAW_SERVO 2
-typedef enum { OPEN = 1024, CLOSED = 0 } claw_pos_t;
+typedef enum { OPEN = 1350, CLOSED = 0 } claw_pos_t;
 void claw(claw_pos_t pos) { set_servo_position(CLAW_SERVO, pos); msleep(500); }
 
 //grab cube from towers
 void grabby () {
+    arm(UP);
     claw(CLOSED);
     arm(DROP);
     grabber_turn(G_TUBE);
@@ -52,10 +53,23 @@ void grabby () {
 }  
 
 #define TUBE_SENSOR 0
-typedef enum { IN = 1660, OUT = 0 } tube_pos; 
-void tube_push (tube_pos pos) { 
-    
+typedef enum { IN = 1600 } tube_pos; 
+void tube_in () { 
+    while (analog(1) < IN) {
+        motor(0, 70);
+        msleep(50);
+    }
+    ao();
 }
+
+void tube_out () {
+    clear_motor_position_counter(0);
+    while (get_motor_position_counter(0) > -500) {
+        motor(0, -50);
+        msleep(100);
+    }
+    ao();
+}    
 
 void wfl() {
     puts("waiting for light");
@@ -67,10 +81,11 @@ void wfl() {
 void setup() {
     enable_servos();
     puts("turn on Create");
-    //create_connect();
-    //tube_push(OUT);
+    create_connect();
     arm(UP);
+    grabber_turn(G_START);
     claw(CLOSED);
+    msleep(400);
     arm(DOWN);
 }
 
@@ -79,41 +94,47 @@ void setup() {
 int main() {
     setup();
     //wfl();
+    msleep(5000);
     shut_down_in(118);
-    //tube push 
     
     //drive to center line
+    drive(SLOW, -SLOW, 280);
+    set_servo_position(3, 1691); msleep(300);
     move(-FAST, -FAST);
-    msleep(2000);
+    tube_in();
+    grabber_turn(G_TOWER);
+    claw(OPEN);
+    msleep(900);
     while (get_create_lcliff_amt() > 2000) {
         move(-FAST, -FAST);
     }
-    arm(UP);
-    grabber_turn(G_TOWER);
-    claw(OPEN);
-    msleep(100); 
+    move(0, 0);
+    msleep(100);
+    drive(SLOW, SLOW, 200);
     
     //rotate 45 deg
-    drive(-SLOW, SLOW, 900);
+    drive(-SLOW, SLOW, 950);
     msleep(100);
-    drive(-FAST, -FAST, 900);
+    drive(-SLOW, -SLOW, 800);
     msleep(100);
     
     //grab botgal and go to drop
     claw(CLOSED);
+    msleep(200);
     arm(DROP);
     //might have to wait for other bot
     drive(FAST, FAST, 950);
     msleep(500);
-    drive(SLOW, -SLOW, 550);
+    drive(-SLOW, SLOW, 1800);
     msleep(500);
     arm(UP);
     claw(OPEN);
-    msleep(500);
+    msleep(300);
     
     //come back and go to next tower
-    drive(-SLOW, SLOW, 600);
+    drive(SLOW, -SLOW, 1900);
     msleep(100);
+    arm(STOW);
     drive(-FAST, -FAST, 1960);
     msleep(100);
     drive(SLOW, SLOW, 500);
@@ -124,7 +145,8 @@ int main() {
     while (get_create_lfcliff_amt() > 2000) {
     	move(-SLOW, -SLOW);
     }
-    drive(-SLOW, -SLOW, 2000);
+    //drive(-SLOW, -SLOW, 2000);
+    drive(-SLOW, -SLOW, 1500);
     msleep(300);
     drive(-SLOW, SLOW, 1250);
     drive(-SLOW, -SLOW, 1000);
@@ -132,7 +154,7 @@ int main() {
     drive(SLOW, SLOW, 300);
     grabby();
     
-    //go to center pipe
+    /*//go to center pipe
     drive(SLOW, SLOW, 500);
     drive(SLOW, -SLOW, 1250);
     while (get_create_lfcliff_amt() > 2000) {
@@ -157,7 +179,7 @@ int main() {
     //noodles
     
     
-    //push out cubes
+    //push out cubes*/
     
     return 0;
 }
