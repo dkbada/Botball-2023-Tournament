7c1eb644d9e93f4079f9c143e6da587ef4068db8
jasonmalik787 <130869805+jasonmalik787@users.noreply.github.com>
2023-07-15 17:30:49 -0400

    Update Roomba.c

--- GCER/Roomba.c
+++ GCER/Roomba.c
@@ -32,52 +32,30 @@ void slow_servo(int which, int to, int delay) {
 }
 
 #define GRABBER_TURN_SERVO 1
-typedef enum { G_TOWER = 0, G_TUBE = 2047, G_START = 1552} grabber_pos_t;
+typedef enum { G_TOWER = 0, G_TUBE = 1900, G_START = 1540} grabber_pos_t;
 void grabber_turn(grabber_pos_t pos) { set_servo_position(GRABBER_TURN_SERVO, pos); msleep(400); }
 
 #define ARM_SERVO 0
-typedef enum { DOWN = 0, UP = 1150, CUBE = 1240, LOW_CUBES = 1814, DROP = 910, STOW = 490, NOODLE = 1400 } arm_pos_t;
+typedef enum { DOWN = 0, UP = 1070, CUBE = 1240, LOW_CUBES = 1814, DROP = 620, STOW = 450, NOODLE = 1400 } arm_pos_t;
+//drop = 910
 void arm(arm_pos_t pos) { slow_servo(ARM_SERVO, pos, 1); }
 
 #define CLAW_SERVO 2
-typedef enum { OPEN = 1350, CLOSED = 0 } claw_pos_t;
+typedef enum { OPEN = 1702, CLOSED = 270 } claw_pos_t;
 void claw(claw_pos_t pos) { set_servo_position(CLAW_SERVO, pos); msleep(500); }
 
 void jiggle() {
     puts("JIGGLE");
     int i;
-    for (i = 0; i < 15; i++) {
+    for (i = 0; i < 10; i++) {
         motor(0, 100); msleep(75);
         motor(0, -100); msleep(75);
     }
     mav(0, 0);
 }
 
-//grab cube from towers
-void grabby () {
-    arm(CUBE);
-    grabber_turn(G_TOWER);
-    claw(CLOSED);
-    msleep(200);
-    arm(STOW);
-    //set_servo_position(0, 490);
-    msleep(500);
-    drive(600, 600, 500);
-    msleep(200);
-    arm(UP);
-    msleep(900);
-    grabber_turn(G_TUBE);
-    msleep(700);
-    arm(DROP);
-    msleep(1500);
-    claw(OPEN);
-    jiggle();
-    arm(STOW);
-    msleep(500);
-    arm(UP);
-}  
 
-typedef enum { IN = 4070, OUT = 100, START = 740 } tube_pos; 
+typedef enum { IN = 4070, OUT = 100, START = 900 } tube_pos; 
 void tube(tube_pos pos) { 
     mav(0, analog(0) < pos ? 1000 : -1000);
     int x;
@@ -87,6 +65,32 @@ void tube(tube_pos pos) {
     } while (x > 5);
     mav(0, 0);
 }
+
+//grab cube from towers
+void grabby () {
+    arm(CUBE);
+    grabber_turn(G_TOWER);
+    claw(CLOSED);
+    msleep(200);
+    arm(DOWN);
+    //set_servo_position(0, 490);
+    drive(600, 600, 500);
+    msleep(200);
+    arm(UP);
+    msleep(900);
+    tube(IN);
+    grabber_turn(G_TUBE);
+    msleep(700);
+    arm(DROP);
+    msleep(1500);
+    claw(OPEN);
+    slow_servo(2, 2047, 1);
+    jiggle();
+    tube(IN);
+    msleep(500);
+    arm(UP);
+    msleep(500);
+}  
 
 void wfl() {
     puts("waiting for light");
@@ -115,25 +119,26 @@ int main() {
     shut_down_in(118);
     
     //drive to center line
-    move(-463, -FAST);
-    //move(-477, -FAST);
+    drive(SLOW, -SLOW, 355);
+    drive(-FAST, -FAST, 1800);
     arm(UP);
-    msleep(500);
     grabber_turn(G_TOWER);
-    move(-FAST, -FAST);
+    claw(OPEN);
+    msleep(500);
+    move(-SLOW, -SLOW);
     until (get_create_lcliff_amt() < 2000);
     move(0, 0);
     msleep(200);
     //drive(SLOW, SLOW, 940);
-    drive(FAST, FAST, 288);
+    //drive(FAST, FAST, 288);
     
     //rotate to tower
-    claw(OPEN);
-    //drive(-SLOW, SLOW, 950);
-    drive(-SLOW, SLOW, 1260);
+    drive(-SLOW, SLOW, 950);
+    //drive(-SLOW, SLOW, 1290);
     msleep(100);
     drive(-SLOW, -SLOW, 2400);
     claw(CLOSED);
+    msleep(500);
     
     //grab botgal and go to drop
     arm(STOW);
@@ -155,49 +160,62 @@ int main() {
     drive(-FAST, -FAST, 1600);
     msleep(100);
     drive(SLOW, SLOW, 900);
-    drive(SLOW, -SLOW, 1250);
-    msleep(100);
+    drive(SLOW, -SLOW, 1180);
     tube(IN);
+    drive(-SLOW, -SLOW, 1800);
+    msleep(100);
     
     //get cube 1
-    move(-SLOW, -SLOW);
+    move(SLOW, SLOW);
     until (get_create_lfcliff_amt() < 2000);
     //drive(-SLOW, -SLOW, 2000);
-    drive(-SLOW, -SLOW, 1400);
+    move(0, 0);
+    msleep(200);
+    //drive(SLOW, SLOW, 5200);
+    drive(FAST, FAST, 1000);
+    drive(SLOW, SLOW, 1000);
     msleep(300);
     drive(-SLOW, SLOW, 1250);
     drive(-SLOW, -SLOW, 1500);
     msleep(500);
     drive(SLOW, SLOW, 200);
+    tube(IN);
     grabby();
-    drive(-SLOW, -SLOW, 300);
+    drive(-SLOW, -SLOW, 3000);
     //drive(FAST, FAST, 200);
     //drive(-FAST, -FAST, 200);
-    drive(-SLOW, -SLOW, 400);
+    drive(SLOW, SLOW, 1400);
     
     //go to center pipe
-    drive(SLOW, -SLOW, 1290);
+    drive(-SLOW, SLOW, 1300);
     move(SLOW, SLOW);
     until (get_create_lfcliff_amt() < 2000);
+    move(0, 0);
+    msleep(200);
     
     //grab cube 2
-    drive(SLOW, SLOW, 4820);
+    //drive(SLOW, SLOW, 4820);
+    //drive(SLOW, SLOW, 3300);
+    drive(FAST, FAST, 700);
+    drive(SLOW, SLOW, 1200);
     grabber_turn(G_TOWER);
     msleep(300);
-    drive(-SLOW, SLOW, 1250);
-    drive(-SLOW, -SLOW, 2500);
+    drive(SLOW, -SLOW, 1250);
+    drive(-SLOW, -SLOW, 3000);
     msleep(300);
     drive(SLOW, SLOW, 100);
+    tube(IN);
     grabby();
-    drive(-SLOW, -SLOW, 600);
+    drive(-SLOW, -SLOW, 2500);
     //drive(FAST, FAST, 200);
     //drive(-FAST, -FAST, 200);
     
     //grab cube 3 (lower cube)
+    drive(SLOW, SLOW, 600);
     msleep(300);
-    drive(SLOW, -SLOW, 1270);
-    drive(SLOW, SLOW, 2900);
-    drive(-SLOW, SLOW, 1250);
+    drive(-SLOW, SLOW, 1270);
+    drive(SLOW, SLOW, 2800);
+    drive(SLOW, -SLOW, 1250);
     drive(-SLOW, -SLOW, 2900);
     drive(SLOW, SLOW, 1000);
     msleep(300);
@@ -206,34 +224,42 @@ int main() {
     drive(SLOW, SLOW, 100);
     claw(CLOSED);
     msleep(200);
-    arm(STOW);
+    arm(UP);
     //set_servo_position(0, 490);
     msleep(500);
     drive(600, 600, 300);
     msleep(200);
-    arm(UP);
-    msleep(200);
     grabber_turn(G_TUBE);
+    tube(IN);
+    arm(UP);
+    msleep(300);
     arm(DROP);
-    msleep(1500);
+    msleep(500);
     claw(OPEN);
     jiggle();
+    arm(250);
+    msleep(400);
+    arm(UP);
+    msleep(400);
+    arm(250);
+    msleep(400);
+    arm(UP);
     drive(-SLOW, -SLOW, 300);
     
     
     //noodles
-    drive(SLOW, SLOW, 400);
-    drive(-SLOW, SLOW, 1270);
-    drive(SLOW, SLOW, 7000);
-    drive(-SLOW, SLOW, 1250);
+    drive(SLOW, -SLOW, 2560);
     msleep(1000);
-    drive(-SLOW, -SLOW, 3000);
+    drive(-FAST, -FAST, 800);
+    drive(-SLOW, -SLOW, 1000);
     drive(SLOW, SLOW, 1000);
-    arm(NOODLE);
+    drive(-SLOW, SLOW, 1250);
+    drive(FAST, FAST, 1580);
     
     
     //push out cubes
     tube(OUT);
+    jiggle();
     
     
     return 0;
